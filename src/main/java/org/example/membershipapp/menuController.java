7f74bfa76a522bd437a8e56e5c2260ed6a8762e2
Sanjaya <117884188+SanjayaCF@users.noreply.github.com>
2024-05-14 16:47:49 +0700

    searchbar by filtering membership name

--- src/main/java/org/example/membershipapp/menuController.java
+++ src/main/java/org/example/membershipapp/menuController.java
@@ -2,6 +2,7 @@ package org.example.membershipapp;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -12,45 +13,61 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javafx.event.ActionEvent;
 
-public class menuController extends switchScenesController{
+public class menuController extends switchScenesController {
 
     private static Connection connection;
 
     public menuController() {
         connection = databaseConnect.getConnection();
     }
-    
+
     public static int userID;
 
     @FXML
     private VBox scrollPaneContent;
+    
+    @FXML
+    private TextField searchField;
 
     public void initialize() throws SQLException {
-        try {
-            double totalHeight = 0.0;
-            
-            final String SQL = "SELECT * FROM memberships WHERE userID = ?";
-            try (PreparedStatement ps = connection.prepareStatement(SQL);) {
-            ps.setInt(1, menuController.userID);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("membershipItems.fxml"));
-                AnchorPane membershipItem = loader.load();
-                membershipItemsController itemController = loader.getController();
-
-                itemController.setName(rs.getString("membershipName"));
-                String currency = rs.getString("currency");
-                itemController.setPrice(currency+" "+rs.getString("price"));
-                itemController.setExpired("Expired "+ rs.getString("dateEnd"));
-                itemController.setCategory(rs.getString("category"));
-                itemController.setBenefit(rs.getString("benefit"));
-                itemController.setPayType(rs.getBoolean("autoPayment")?"Auto Paid":"Paid");
-                itemController.setInterval(rs.getString("paymentInterval"));
-                scrollPaneContent.getChildren().add(membershipItem);
-
-                totalHeight += membershipItem.getPrefHeight();
+        loadMemberships("");
+        
+        searchField.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+                loadMemberships(newValue);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-            
+        });
+    }
+
+    private void loadMemberships(String filter) throws SQLException {
+        try {
+            scrollPaneContent.getChildren().clear();
+            double totalHeight = 0.0;
+
+            final String SQL = "SELECT * FROM memberships WHERE userID = ? AND membershipName LIKE ?";
+            try (PreparedStatement ps = connection.prepareStatement(SQL);) {
+                ps.setInt(1, menuController.userID);
+                ps.setString(2, "%" + filter + "%");
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("membershipItems.fxml"));
+                    AnchorPane membershipItem = loader.load();
+                    membershipItemsController itemController = loader.getController();
+
+                    itemController.setName(rs.getString("membershipName"));
+                    String currency = rs.getString("currency");
+                    itemController.setPrice(currency + " " + rs.getString("price"));
+                    itemController.setExpired("Expired " + rs.getString("dateEnd"));
+                    itemController.setCategory(rs.getString("category"));
+                    itemController.setBenefit(rs.getString("benefit"));
+                    itemController.setPayType(rs.getBoolean("autoPayment") ? "Auto Paid" : "Paid");
+                    itemController.setInterval(rs.getString("paymentInterval"));
+                    scrollPaneContent.getChildren().add(membershipItem);
+
+                    totalHeight += membershipItem.getPrefHeight();
+                }
             }
 
             scrollPaneContent.setPrefHeight(totalHeight);
@@ -58,8 +75,7 @@ public class menuController extends switchScenesController{
             e.printStackTrace();
         }
     }
-    
-    
+
     @FXML
     protected void SwitchToAddMembership(ActionEvent event) throws IOException {
         switchToAddMembershipPage(event);
@@ -95,4 +111,3 @@ public class menuController extends switchScenesController{
         switchToAccount(event);
     }
 }
-

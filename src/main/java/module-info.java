9b5ac97ff8bafcd8b45ffaf9bd296e44a892b07d
71220840 - Rendy Ananta Kristanto <rendy.ananta@ti.ukdw.ac.id>
2024-05-31 14:22:01 +0700

    perbaikan penempatan file2 yang kacau tidak sesuai folder

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.week10 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens org.example.membershipapp to javafx.fxml;
-    exports org.example.membershipapp;
-}
-
